9c5aff7a4f8237d6a80a02c1221633a1be403e4b
ongsterr <ong.chris11@gmail.com>
2018-11-04 10:25:47 +1100

    test: add more test for api

--- server/src/__tests__/api.test.js
+++ server/src/__tests__/api.test.js
@@ -1,9 +1,26 @@
 const api = require('../api')
 const { apiConfig } = require('../config')
 
-describe('Basiq API authentication', () => {
-	it('should return access token', async () => {
-		const response = await api.Auth.getToken(apiConfig.key)
-		expect(response.access_token).toHaveLength(353)
+describe('Basiq API testing', () => {
+	let token
+
+	describe('Basiq API authentication', () => {
+		it('should return access token', async () => {
+			const response = await api.Auth.getToken(apiConfig.key)
+			token = response.access_token
+			expect(token).toHaveLength(353)
+		})
+	})
+
+	describe('Basiq API user object testing', () => {
+		const testAccount = {
+			email: 'gavin@hooli.com',
+			mobile: '+61410888666',
+		}
+
+		it('should create a user', async () => {
+			const response = await api.Users.create(token, testAccount)
+			expect(response.type).toBe('user')
+		})
 	})
 })
