768666430b7e4310ecec47221bccd5bc6c5796c0
ongsterr <ong.chris11@gmail.com>
2018-11-04 16:07:49 +1100

    test: complete user api testing

--- server/src/__tests__/api.test.js
+++ server/src/__tests__/api.test.js
@@ -18,9 +18,35 @@ describe('Basiq API testing', () => {
 			mobile: '+61410888666',
 		}
 
+		let userId
+
 		it('should create a user', async () => {
 			const response = await api.Users.create(token, testAccount)
+			userId = response.id
 			expect(response.type).toBe('user')
+			expect(response.email).toBe(testAccount.email)
+			expect(response.mobile).toBe(testAccount.mobile)
+		})
+
+		it('should retrieve a user', async () => {
+			const response = await api.Users.retrieve(token, userId)
+			expect(response.type).toBe('user')
+			expect(response.email).toBe(testAccount.email)
+			expect(response.mobile).toBe(testAccount.mobile)
+		})
+
+		it('should update details of a user', async () => {
+			const updatedData = {
+				mobile: '+61412345678',
+			}
+			const response = await api.Users.update(token, userId, updatedData)
+			expect(response.type).toBe('user')
+			expect(response.mobile).toBe(updatedData.mobile)
+		})
+
+		it('should delete a user', async () => {
+			const response = await api.Users.delete(token, userId)
+			expect(response).toBe(204)
 		})
 	})
 })
